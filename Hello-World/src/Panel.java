2eff81f023b59e1262dd134bc880ea69ae336584
leon <leon@leon-pc>
2018-03-15 20:21:41 +0100

    test

--- Hello-World/src/Panel.java
+++ Hello-World/src/Panel.java
@@ -1,16 +1,11 @@
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel{
-	 private Image bardejov;
 	 private BufferedImage image;
 
 	//Leon
@@ -20,26 +15,11 @@ public class Panel extends JPanel{
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		initBoard();
 	}
-	   private void initBoard() {
-	        
-	        loadImage();
-	        
-	        int w = bardejov.getWidth(this);
-	        int h =  bardejov.getHeight(this);
-	        setPreferredSize(new Dimension(w, h));        
-	    }
-	private void loadImage() {
-	        
-	        ImageIcon ii = new ImageIcon("bg.png");
-	        bardejov = ii.getImage();        
-	    }
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	     g.fillRect(100,100,150,250);
-	     g.drawImage(bardejov, 0, 0, null);
 	     g.drawImage(image, 100, 100,1000,1000, null);
 	}
  
