105139e0584cf31e9a4df42643f7daf275cf6ea1
Peter <trompeten.peter@gmx.de>
2018-03-15 19:06:43 +0100

    dfgh

--- Hello-World/src/Frame.java
+++ Hello-World/src/Frame.java
@@ -1,16 +1,31 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame {
 	//peter
 	public Frame() {
 		initFrame();
+		pan();
 		
 	}
 	private void initFrame() {
-		setTitle("flappy");
+		setTitle(Konst.TITLE);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(200, 200);
+		setSize(Konst.WIDTH, Konst.HEIGHT);
+		
+		
 		
 	}
+	private void pan() {
+		JPanel panel = new JPanel();
+		
+		panel.setBackground(Color.red);
+		
+		
+		add(panel);
+	}
+
 }
--- Hello-World/src/Konst.java
+++ Hello-World/src/Konst.java
@@ -1,5 +1,7 @@
 
 public class Konst {
+	//peter
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 800;
+	public static final String TITLE = "flappy";
 }
