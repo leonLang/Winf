37c5b77a0bce85bf7391e7b92a5b0f0f55766c77
leon <leon@leon-pc>
2018-03-15 20:19:25 +0100

    new

--- Hello-World/src/Panel.java
+++ Hello-World/src/Panel.java
@@ -1,15 +1,25 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel{
 	 private Image bardejov;
-	 
+	 private BufferedImage image;
+
 	//Leon
 	public Panel() {
+		try {
+		image = ImageIO.read(getClass().getResourceAsStream("bg.png"));
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 		initBoard();
 	}
 	   private void initBoard() {
@@ -29,7 +39,8 @@ public class Panel extends JPanel{
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	     g.fillRect(100,100,150,250);
-	     g.drawImage(bardejov, 100, 100, null);
+	     g.drawImage(bardejov, 0, 0, null);
+	     g.drawImage(image, 100, 100,1000,1000, null);
 	}
  
 }
