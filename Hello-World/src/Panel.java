599e25bd663c389bd5269b77d8bcd832ea9decf7
leon <leon@leon-pc>
2018-03-15 20:08:18 +0100

    xxxxxxxxxxxxxxxxxx

--- Hello-World/src/Panel.java
+++ Hello-World/src/Panel.java
@@ -1,15 +1,35 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Panel extends JPanel{
+	 private Image bardejov;
+	 
 	//Leon
 	public Panel() {
-	
+		initBoard();
 	}
+	   private void initBoard() {
+	        
+	        loadImage();
+	        
+	        int w = bardejov.getWidth(this);
+	        int h =  bardejov.getHeight(this);
+	        setPreferredSize(new Dimension(w, h));        
+	    }
+	private void loadImage() {
+	        
+	        ImageIcon ii = new ImageIcon("bg.png");
+	        bardejov = ii.getImage();        
+	    }
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	     g.fillRect(100,100,150,250);
+	     g.drawImage(bardejov, 100, 100, null);
 	}
+ 
 }
