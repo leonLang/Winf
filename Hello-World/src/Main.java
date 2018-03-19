55b459a50e93f8276886622d2245fcf7615328ca
leon <leon@leon-pc>
2018-03-19 19:12:50 +0100

    SSSSSSSSSSSSS

--- Hello-World/src/Main.java
+++ Hello-World/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new  Frame();
+		while (true) {
+            System.out.println("klappt");
+            
+        }
 	}
+	
 
 }
