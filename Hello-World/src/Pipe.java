0b35d4a5c20a7411a1d1e034215689f946a97d6c
leon <leon@leon-pc>
2018-03-20 16:54:10 +0100

    ssssssssssss

--- Hello-World/src/Pipe.java
+++ Hello-World/src/Pipe.java
@@ -6,12 +6,16 @@ import javax.imageio.ImageIO;
 
 public class Pipe {
 	//Leon
-	public static int x = 700;
-	public static int x1 = 1000;
-	public static int x2 = 1300;
-	public static int y = 100;
-	public static int y1 = 100;
-	public static int y2 = 100;
+	public static int x1 = 700;
+	public static int x2 = 1000;
+	public static int x3 = 1300;
+	
+	public static int y1 = -100;
+	public static int y2 = 700;
+	public static int y3 = -100;
+	public static int y4 = 700;
+	public static int y5 = -100;
+	public static int y6 = 700;
 
 	private static BufferedImage pipe1;
 	private static BufferedImage pipe2;
@@ -24,16 +28,28 @@ public class Pipe {
 			}
 		}
 	
-	public static void paintP(Graphics g,int x,int y) {
-		g.drawImage(pipe1, x,y,100,500, null);
-		g.drawImage(pipe2, x,y,100,500, null);	
+	public static void paintP(Graphics g,int x1,int y1, int y2) {
+		g.drawImage(pipe1, x1,y1,100,500, null);
+		g.drawImage(pipe2, x1,y2,100,500, null);	
 	}
 	
 	public static void generate(Graphics g) {
-		x = x-1;
-		paintP(g, x, y);
-		paintP(g, x1, y1);
-		paintP(g, x2, y2);
+		if (x1<-100){
+			x1=800;
+		}
+		if (x2<-100){
+			x2=800;
+		}
+		if (x3<-100){
+			x3=800;
+		}
+		x1 = x1-1;
+		x2 = x2-1;
+		x3 = x3-1;
+
+		paintP(g, x1, y1,y2);
+		paintP(g, x2, y3,y4);
+		paintP(g, x3, y5,y6);
 	}
 	
 }
