68778b68e228f982928fa117b9cf48317515fb14
leon <leon@leon-pc>
2018-03-20 17:35:35 +0100

    ssssss

--- Hello-World/src/TextDisplay.java
+++ Hello-World/src/TextDisplay.java
@@ -1,11 +1,13 @@
 import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
 
 import javax.swing.JLabel;
 
 public class TextDisplay extends JLabel{
-	//peter
-	public TextDisplay(int x, int y,String title) {
-		setLocation(x,y);
-		setText(title);
+		//Leon
+	public static void font(Graphics g) {
+		Font font = new Font("Serif", Font.BOLD, 36);
+	     g.drawString("hello", 100, 100);
 	}
 }
